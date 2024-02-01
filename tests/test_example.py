57c7dd8d1f86768e9287bab68782b3082ba85105
mbaxter <mbaxter.dev@gmail.com>
2024-02-01 11:30:29 -0500

    Rework dependency management - use poetry (#3)

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,16 @@
-def test_is_empty_str_none():
-    assert True
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+import unittest
+
+from package_name.example import add_one
+
+
+class TestSimple(unittest.TestCase):
+    def test_add_one(self):
+        self.assertEqual(add_one(5), 6)
+
+
+if __name__ == "__main__":
+    unittest.main()
